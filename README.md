7b7936d679d747b731cba1d8253d412833cff0a0
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2023-05-05 17:21:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Estimated Read Time
+
+Adds the estimated read time of a note.
+
+## Features
+
+- Display as...
+  - Callout
+  - Status Bar
+  - View
+- Toggle Button in Ribbon
+- Read Speed (default 200wpm)
+- Callout type
+- Callout text before
+- Callout text after
---
+++ main.ts
@@ -0,0 +1,34 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+		const readTimeStatus = this.addStatusBarItem();
+		readTimeStatus.createEl("span", { text: "Hello!" })
+		console.log(this.app.workspace.getActiveViewOfType(MarkdownView)?.getViewData())
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
